89a575cb9286c40b6c08a0fbaa4bb3b8470f6c7a
Simon Brand <tartanllama@gmail.com>
2017-03-26 09:40:47 +0100

    Fix breakpoint size bug

--- include/breakpoint.hpp
+++ include/breakpoint.hpp
@@ -11,16 +11,20 @@ namespace minidbg {
         breakpoint(pid_t pid, std::intptr_t addr) : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{} {}
 
         void enable() {
-            m_saved_data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+            auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+            m_saved_data = static_cast<uint8_t>(data & 0xff); //save bottom byte
             uint64_t int3 = 0xcc;
-            uint64_t data_with_int3 = ((m_saved_data & ~0xff) | int3); //set bottom byte to 0xcc
+            uint64_t data_with_int3 = ((data & ~0xff) | int3); //set bottom byte to 0xcc
             ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
 
             m_enabled = true;
         }
 
         void disable() {
-            ptrace(PTRACE_POKEDATA, m_pid, m_addr, m_saved_data);
+            auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+            auto restored_data = ((data & ~0xff) | m_saved_data);
+            ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
+
             m_enabled = false;
         }
 
@@ -31,7 +35,7 @@ namespace minidbg {
         pid_t m_pid;
         std::intptr_t m_addr;
         bool m_enabled;
-        uint64_t m_saved_data; //data which used to be at the breakpoint address
+        uint8_t m_saved_data; //data which used to be at the breakpoint address
     };
 }
 
